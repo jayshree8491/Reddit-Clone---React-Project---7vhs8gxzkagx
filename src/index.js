75fe2746694b3935cae44ce7cbb6b7aefa477221
jayshree8491 <pali.jayshree@gmail.com>
2023-04-22 17:35:17 +0530

    ns

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {BrowserRouter } from 'react-router-dom'
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
 
-
-ReactDOM.render(<App />, document.getElementById("root"));
+reportWebVitals();
