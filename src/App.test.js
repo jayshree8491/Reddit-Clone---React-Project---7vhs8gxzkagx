77b8b9f9fe96d620ec41171225311c69c0fbc9cd
jayshree8491 <119621789+jayshree8491@users.noreply.github.com>
2023-05-02 23:04:01 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import Posts from './Posts';
+
+test('renders a list of posts', async () => {
+  // Render the Posts component
+  render(<Posts />);
+
+  // Wait for the component to load
+  const postsList = await screen.findByRole('list');
+
+  // Check that there are at least two posts in the list
+  const posts = postsList.querySelectorAll('li');
+  expect(posts.length).toBeGreaterThanOrEqual(2);
 });
