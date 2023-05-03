8e7af6d0f558d45631db74eebfcb5e647980812b
jayshree8491 <119621789+jayshree8491@users.noreply.github.com>
2023-05-03 18:11:02 +0530

    Update post.services.js

--- src/services/post.services.js
+++ src/services/post.services.js
@@ -5,9 +5,10 @@ import { collection, getDocs, addDoc, updateDoc,doc} from "firebase/firestore"
 
 const postCollectiopnRef=collection(db,"posts")
 class postDataService{
-    addPost=(newPost)=>{
-        return addDoc(postCollectiopnRef,newPost)
-    }
+  addPost = (newPost) => {
+  console.log("newPost:", newPost);
+  return addDoc(postCollectionRef, newPost);
+};
 
     updatePost=(id,updatedPost)=>{
         const postDoc=doc(db,"posts",id)
