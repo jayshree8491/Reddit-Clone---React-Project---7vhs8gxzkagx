2264933b481a443329cb34c286d06c9fdd5a250a
jayshree8491 <119621789+jayshree8491@users.noreply.github.com>
2023-05-03 17:13:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,32 +7,22 @@ import Posts from './components/Posts';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-  };
-
-  const handleLogout = () => {
-    setIsLoggedIn(false);
-  };
-
   const [addpostPopup,setAddPostPopup]=useState(false)
   const [loginPopup,setLoginPopup]=useState(false)
-
   return (
     <>
-      <div className="App">
-        <Navbar isLoggedIn={isLoggedIn} handleLogout={handleLogout} setPostTrigger={setAddPostPopup} setLoginTrigger={setLoginPopup}/>
-      </div>
-      <div className='routes'>
-        <Routes>
-          <Route path='/' element={<Posts/>}/>
-          <Route path='/login' element={<LoginPopup trigger={loginPopup} setLoginTrigger={setLoginPopup} handleLogin={handleLogin} />} />
-          <Route path='/addpost' element={<AddPostPopup trigger={addpostPopup} setPostTrigger={setAddPostPopup}/>}/>
-          ...
-        </Routes>
-      </div>
+    <div className="App">
+      <Navbar setPostTrigger={setAddPostPopup} setLoginTrigger={setLoginPopup}/>
+      
+    </div>
+    <div className='routes'>
+      <Routes>
+        <Route path='/' element={<Posts/>}/>
+        <Route path='/login' element={<LoginPopup trigger={loginPopup} setLoginTrigger={setLoginPopup}/>}/>
+        <Route path='/addpost' element={<AddPostPopup trigger={addpostPopup} setPostTrigger={setAddPostPopup}></AddPostPopup>}/>
+      </Routes>
+    
+    </div>
     </>
   );
 }
