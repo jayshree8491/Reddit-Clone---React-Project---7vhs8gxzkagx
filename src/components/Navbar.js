92d4405df02b8b3ffc4ce617bfd0cdca5e165cf7
jayshree8491 <119621789+jayshree8491@users.noreply.github.com>
2023-05-03 16:30:01 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,51 +7,42 @@ import "../stylesheets/logo.css"
 import logo from "./logo.png"
 
 
-const Navbar = (props) => {
+function Navbar(props) {
   const navigate = useNavigate();
 
-  const handleLogout = () => {
-    localStorage.removeItem('userName');
-    props.handleLogout();
-    navigate('/login');
+  const addPost = () => {
+    navigate("/addpost");
   };
-  const addPost=()=>{
-    props.setPostTrigger(true)
-    navigate('/addpost')
-  }
-  const login=()=>{
-    props.setLoginTrigger(true)
-    navigate('/login')
-  }
-  const logout=()=>{
-    localStorage.removeItem('userName')
-    toast('Logged out successfully')
-    navigate('/login')
-  }
+
+  const login = () => {
+    navigate("/login");
+  };
+
+  const logout = () => {
+    localStorage.removeItem("userName");
+    toast("Logged out successfully");
+    navigate("/login");
+  };
+
   return (
     <>
-    <div className='nav'>
-        <span id='reddit-logo' > <img src={logo} alt="Logo" /></span>
-        <span id='login'>
-          {isLoggedIn() && <button onClick={addPost}>Add post</button>}
-          {!isLoggedIn() && <button onClick={login}>Login</button>}
-          {isLoggedIn() && <button onClick={logout}>Logout</button>}
+      <div className="nav">
+        <span id="reddit-logo">
+          <img src={logo} alt="Logo" />
         </span>
-         <nav>
-      <Link to="/">Home</Link>
-      {props.isLoggedIn ? (
-        <>
-          <button onClick={handleLogout}>Logout</button>
-          <Link to="/addpost">Add Post</Link>
-        </>
-      ) : (
-        <Link to="/login">Login</Link>
-      )}
-    </nav>
-    </div>
-    <ToastContainer/>
+        <span id="login">
+          {props.isLoggedIn ? (
+            <>
+              <button onClick={addPost}>Add post</button>
+              <button onClick={logout}>Logout</button>
+            </>
+          ) : (
+            <button onClick={login}>Login</button>
+          )}
+        </span>
+      </div>
+      <ToastContainer />
     </>
-  )
+  );
 }
-
 export default Navbar;
