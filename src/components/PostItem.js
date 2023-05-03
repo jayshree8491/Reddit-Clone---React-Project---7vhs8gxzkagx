520ea295bf6e4194bae84cdaa7a951beceab31f2
jayshree8491 <119621789+jayshree8491@users.noreply.github.com>
2023-05-03 16:34:28 +0530

    Update PostItem.js

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -5,93 +5,72 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const PostItem = (props) => {
-  const { id, title, body, upVotes, downVotes } = props.post;
+  const { id, postText, upVotes, downVotes } = props.post;
   const { handleUpVote, handleDownVote } = props;
 
-  let [prevUpVote, setPrevUpVote] = useState(upVotes);
-  let [prevDownVote, setPrevDownVote] = useState(downVotes);
-  const [upVote, setUpVote] = useState(upvote);
-  const [downVote, setDownVote] = useState(downvote);
+  const [prevUpVote, setPrevUpVote] = useState(upVotes);
+  const [prevDownVote, setPrevDownVote] = useState(downVotes);
+  const [upVote, setUpVote] = useState(upVotes);
+  const [downVote, setDownVote] = useState(downVotes);
   const [userVoted, setUserVoted] = useState([]);
+  const user = localStorage.getItem('userName');
 
-
-
-  // console.log(postId)
-  
-  const handleUpVote = () => {
-    setPrevUpVote(prevUpVote + 1);
-    // ...
-  }
-
-  const handleDownVote = () => {
-    setPrevDownVote(prevDownVote + 1);
-    // ...
-  }
-
-  
-  
-  const isLoggedin=()=>{
-    if(localStorage.getItem('userName')){
-      return true;
-    }
-    return false;
-  }
   const addUpVote = async (e) => {
-  if (isLoggedin() && !userVoted.includes(user)) {
-    setUpVote((prevUpVote) => prevUpVote + 1);
-    setUserVoted((prevUserVoted) => [...prevUserVoted, user]);
-    const updatedPost = {
-      post: postText,
-      upvote: prevUpVote + 1, // use the previous state value
-      downvote: downVote,
-      uservoted: [...userVoted, user],
-    };
-    try {
-      await postDataService.updatePost(postId, updatedPost);
-    } catch (err) {
-      toast(err.message);
+    if (user && !userVoted.includes(user)) {
+      setUpVote((prevUpVote) => prevUpVote + 1);
+      setUserVoted((prevUserVoted) => [...prevUserVoted, user]);
+      const updatedPost = {
+        post: postText,
+        upvote: prevUpVote + 1,
+        downvote: downVote,
+        uservoted: [...userVoted, user],
+      };
+      try {
+        await postDataService.updatePost(id, updatedPost);
+      } catch (err) {
+        toast(err.message);
+      }
+    } else if (!user) {
+      toast("Please login 🙏 to Upvote");
     }
-  } else if (!isLoggedin()) {
-    toast("Please login 🙏 to Upvote");
-  }
-};
+  };
 
-const addDownVote = async (e) => {
-  if (isLoggedin() && !userVoted.includes(user)) {
-    setDownVote((prevDownVote) => prevDownVote + 1);
-    setUserVoted((prevUserVoted) => [...prevUserVoted, user]);
-    const updatedPost = {
-      post: postText,
-      upvote: upVote,
-      downvote: prevDownVote + 1, // use the previous state value
-      uservoted: [...userVoted, user],
-    };
-    try {
-      await postDataService.updatePost(postId, updatedPost);
-    } catch (err) {
-      toast(err.message);
+  const addDownVote = async (e) => {
+    if (user && !userVoted.includes(user)) {
+      setDownVote((prevDownVote) => prevDownVote + 1);
+      setUserVoted((prevUserVoted) => [...prevUserVoted, user]);
+      const updatedPost = {
+        post: postText,
+        upvote: upVote,
+        downvote: prevDownVote + 1,
+        uservoted: [...userVoted, user],
+      };
+      try {
+        await postDataService.updatePost(id, updatedPost);
+      } catch (err) {
+        toast(err.message);
+      }
+    } else if (!user) {
+      toast("Please login 🙏 to Downvote");
     }
-  } else if (!isLoggedin()) {
-    toast("Please login 🙏 to Downvote");
-  }
-};
+  };
 
   return (
     <>
-    <div className='postItem-container'>
-      <div className='vote'>
-        <button onClick={addUpVote} className='upvoteBtn'>↑</button>
-        <button onClick={addDownVote} className='downvoteBtn'>↓</button>
-        <div className='voteCount'>{upVote}</div>
-        <div className='voteCount'>{downVote}</div>
+      <div className='postItem-container'>
+        <div className='vote'>
+          <button onClick={addUpVote} className='upvoteBtn'>↑</button>
+          <button onClick={addDownVote} className='downvoteBtn'>↓</button>
+          <div className='voteCount'>{upVote}</div>
+          <div className='voteCount'>{downVote}</div>
+        </div>
+        <div className='postInfo'>
+          <div>{postText}</div>
+        </div>
       </div>
-      <div className='postInfo'>
-        <div>{postText}</div>
-      </div>
-    </div>
-    <ToastContainer/>
+      <ToastContainer />
     </>
-  )
+  );
 }
 
 export default PostItem;
