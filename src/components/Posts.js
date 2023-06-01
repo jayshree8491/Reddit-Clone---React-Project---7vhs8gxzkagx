fcbadcef019f7c77448e8501e56c5d423b4a3017
jayshree8491 pal <pali.jayshree@gmail.com>
2023-06-01 09:52:05 +0000

    n

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,39 +1,47 @@
-import React, { useEffect, useState } from 'react'
-import PostItem from './PostItem'
-import postDataService from '../services/post.services'
-import "../stylesheets/post.css"
+import React, { useEffect, useState } from 'react';
+import PostItem from './PostItem';
+import postDataService from '../services/post.services';
+import '../stylesheets/post.css';
 
 const Posts = () => {
-    const [posts,setPosts]=useState([])
-    useEffect(()=>{
-        getPosts()
-    },[])
-   
-    
-    const getPosts = async () => {
-  const data = await postDataService.getAllPosts();
-  setPosts(prevPosts => {
-    return data.docs.map(doc => ({ ...doc.data(), id: doc.id }));
-  });
-};
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  const getPosts = async () => {
+    try {
+      const data = await postDataService.getAllPosts();
+      setPosts(prevPosts => {
+        return data.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+      });
+      setLoading(false); // Set loading state to false when data is fetched
+    } catch (error) {
+      console.error('Error fetching posts:', error);
+      setLoading(false); // Set loading state to false even if an error occurs
+    }
+  };
 
   return (
     <div className="post-container">
-      {/* <pre>{JSON.stringify(posts,undefined,1)}</pre> */}
-      {posts.map((post,index)=>{
-        return(
-            <PostItem 
-                key={post.id}
-                postText={post.post}
-                upvote={post.upvote}
-                downvote={post.downvote}
-                userVoted={post.uservoted}
-                postId={post.id}
-            />
-        )
-      })}
+      {loading ? (
+        <div>Loading...</div> // Display a loading message or spinner while data is being fetched
+      ) : (
+        posts.map(post => (
+          <PostItem
+            key={post.id}
+            postText={post.post}
+            upvote={post.upvote}
+            downvote={post.downvote}
+            userVoted={post.uservoted}
+            postId={post.id}
+          />
+        ))
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Posts
+export default Posts;
