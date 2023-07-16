71e8e5ecb84781183f262e300871bd58df6671f8
jayshree <119621789+jayshree8491@users.noreply.github.com>
2023-07-16 15:19:05 +0000

    Update 4 files

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,15 +1,27 @@
-import React from 'react';
-
+import React from "react";
 
 function Article(props) {
-  let base_url = 'https://reddit.com';
+  let base_url = "https://reddit.com";
+
+  // Check if props.article.posts is defined and an array
+  if (!props.article.posts || !Array.isArray(props.article.posts)) {
+    return null; // or render an appropriate fallback
+  }
 
   return (
     <div className="subreddit-bar">
-      <h3 className="subreddit-title">{props.article.subreddit_name_prefixed}</h3>
+      <h3 className="subreddit-title">
+        {props.article.subreddit_name_prefixed}
+      </h3>
       <div className="subreddit-posts">
         {props.article.posts.map((post) => (
-          <a key={post.id} href={base_url + post.permalink} target="_blank" className="subreddit-post">
+          <a
+            key={post.id}
+            href={base_url + post.permalink}
+            target="_blank"
+            className="subreddit-post"
+            rel="noreferrer"
+          >
             <span className="subreddit-post-title">{post.title}</span>
             <span className="subreddit-post-score">Score: {post.score}</span>
           </a>
